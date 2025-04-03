1a47042c2df92126e70feaa8df2b22e609515793
Андрей Колчин <a.kolchin@egov66.ru>
2025-04-03 23:41:17 +0500

    del: venv deleted

--- client/static/scripts/script.js
+++
@@ -1,7 +0,0 @@
-
-function get_data() {
-    $.getJSON('http://172.20.0.5:9000/', function(data) {
-        display = document.getElementsByTagName('textarea')[0]
-        display.textContent = 'Время на сервере: ' + data["server_time"];
-      });
-}
