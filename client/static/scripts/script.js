8967aa519d3b6dcb494d7c25d036ce6d37a8ab50
Agamali <muhammad.agamalieff@yandex.ru>
2025-04-04 11:47:48 +0500

    Laba3

--- client/static/scripts/script.js
+++ client/static/scripts/script.js
@@ -2,6 +2,6 @@
 function get_data() {
     $.getJSON('http://172.20.0.5:9000/', function(data) {
         display = document.getElementsByTagName('textarea')[0]
-        display.textContent = 'Время на сервере: ' + data["server_time"];
+        display.textContent = 'Ответ сервера: ' + data["response"];
       });
 }
--- server/app.py
+++ server/app.py
@@ -17,7 +17,7 @@ app.add_middleware(
 
 @app.get('/')
 async def root():
-    return {'server_time': datetime.now(ZoneInfo('Asia/Yekaterinburg'))}
+    return {'response': "Проверка связи"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=9000)
